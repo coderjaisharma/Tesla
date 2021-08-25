d77a5ca243e723eeaf4a707bf3d92752969a66ff
Jai <jai@jaiks.in>
2021-08-25 20:48:52 +0530

    changes

---
+++ src/component/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button.css'
+const Button = ({imp,text,link}) => {
+    return (<div>
+        <div className={`button ${imp==='secondary'?'button__white':''}`}>
+        <a href={link}>{text}</a>
+        </div>
+    </div>)
+}
+export default Button
